e7385454bc0721b44f7d80508eb94969f889df3e
chrizxzayax <chmolina629@hotmail.com>
2025-10-03 00:06:57 -0700

    start fixing destructor to free memory

--- lab19.cpp
+++ lab19.cpp
@@ -22,15 +22,34 @@ class MovieReviews {
             
     public:
 
-    MovieReviews(const string& t) : title(t), head(nullptr) {}
+        MovieReviews(const string& t) : title(t), head(nullptr) {}
 
-    void addHead(double rating, const string& comment) {
-        ReviewNode* newNode = new ReviewNode(rating, comment);
-        newNode->next = head;
-        head = newNode;
-    }
+        void addHead(double rating, const string& comment) {
+            ReviewNode* newNode = new ReviewNode(rating, comment);
+            newNode->next = head;
+            head = newNode;
+        }
 
+        void printRev() const {
+            cout << "Movie: " << title << endl;
+            ReviewNode* current = head;
+            int count = 1;
+            while (current) {
+                cout << "  > Review " << count++ << ": " << fixed << setprecision(1) << current->rating 
+                     << " - " << current->comment << endl;
+                current = current->next;
+            }
+        }
 
+        ~MovieReviews() {// destructor to free memory
+            ReviewNode* current = head;
+            while (current) {
+                ReviewNode* temp = current->next;
+                delete current;
+                current = temp;
+            }
+            head = nullptr;
+        }
 };
 
 vector<string> readComments(const string& filename) {
@@ -47,6 +66,7 @@ vector<string> readComments(const string& filename) {
             comments.push_back(line);
         }
     }
+    fin.close();
     return comments;
 }
 
